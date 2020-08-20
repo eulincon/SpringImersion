842d74488867664822070b36819694226a68eb77
Lincon Brito <38511541+zlincon@users.noreply.github.com>
2020-08-20 14:05:43 -0300

    #

--- osworks-api/src/main/java/com/springproject/osworks/api/controller/ClienteController.java
+++ osworks-api/src/main/java/com/springproject/osworks/api/controller/ClienteController.java
@@ -1,31 +1,76 @@
 package com.springproject.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springproject.osworks.domain.model.Cliente;
+import com.springproject.osworks.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
-	@GetMapping("/clientes")
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@GetMapping
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("João de");
-		cliente1.setTelefone("88 99999-4545");
-		cliente1.setEmail("joao@gmail.com");
+		return clienteRepository.findAll();
+	}
+
+	@GetMapping("{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+
+		if (cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionanr(@Valid @RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("{clienteId}")
+	public ResponseEntity<Cliente> atualizar(@Valid @PathVariable Long clienteId, @RequestBody Cliente cliente){
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
 		
-		var cliente2 = new Cliente();
-		cliente2.setId((long) 2);
-		cliente2.setNome("Maria");
-		cliente2.setTelefone("88 89999-4568");
-		cliente2.setEmail("maria@gmail.com");
+		cliente.setId(clienteId);
+		cliente = clienteRepository.save(cliente);
 		
-		return Arrays.asList(cliente1, cliente2);
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("{clienteId}")
+	public ResponseEntity<Void> remover(@PathVariable Long clienteId) {
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		clienteRepository.deleteById(clienteId);
+		
+		return ResponseEntity.noContent().build();
 	}
 
 }
---
+++ osworks-api/src/main/java/com/springproject/osworks/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.springproject.osworks.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		var problema = new Problema();
+		problema.setStatus(status.value());
+		problema.setTitulo("Um ou mais campos estão inválidos. Preencha corretamente e tente novamente.");
+//		problema.setTitulo(ex.getBindingResult().getFieldError().getDefaultMessage());
+		problema.setDataHora(LocalDateTime.now());
+		
+//		return super.handleMethodArgumentNotValid(ex, headers, status, request);
+		return super.handleExceptionInternal(ex, problema, headers, status, request);
+	}
+}
